6f836fb4108084ebcfb2f6b2b8f86d7642b00f13
ddaatts1 <dodat743@gmail.com>
2024-04-04 16:43:27 +0700

    update 1 :add mysql database connection

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require("express")
+const init_db = require("./services/testdb")
+const product_db = require("./database_config/database_connect")
+const express = require("express");
+const schedule_task = require("./services/schedule_task")
+const app = express();
 
-const app =express()
+const data = [
+    ['iphone 10', 464000]
 
-
-app.get("/" ,(req,res)=>{
-    res.send("<h1>welcome to nodejs</h1>")
+];
+product_db().query('INSERT INTO phones (name, price) VALUES ?', [data], (error, results) => {
+    if (error) throw error;
+    console.log(`${results.affectedRows} rows inserted`);
 })
-const port = process.env.PORT ||3000
+// app.get("/", (req, res) => {
+//     res.send("<h1>Welcome to Node.js</h1>");
+// });
 
-app.listen(port,()=> console.log(`Listening on port ${port}`))
+app.get("/", (req, res) => {
+    // Execute the SELECT * query
+    product_db().query('SELECT * FROM phones', (error, results) => {
+        if (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
 
+        // Send the results as JSON
+        res.json(results);
+    });
+
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
